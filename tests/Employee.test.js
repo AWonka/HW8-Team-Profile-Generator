ec862bcc1210112c82032183b5b8fc2a71a48f59
Austin Wonka <wonkaaustin@gmail.com>
2022-01-11 12:16:27 -0500

    finished testing creating an employee object with properties

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -5,8 +5,29 @@ describe('Employee', () => {
         const employee = new Employee();
         
         expect(typeof(employee)).toBe('object');
+    });
 
-        
+    it('Gives a name property to the employee object', () => {
+        const name = 'Austin';
+        const employee = new Employee(name);
 
-    })
-})
+        expect(employee.name).toBe('Austin');
+    });
+
+    it('Gives an id property to the employee object', () => {
+        const id = 25;
+        // give a random name as first value to cover .name property
+        const employee = new Employee('Austin', id);
+
+        expect(employee.id).toBe(25);
+    });
+
+    it('Gives an email property to the employee object', () => {
+        const email = 'wonkaaustin@gmail.com';
+        // give random name and id property to fill .name and .id properties
+        const employee = new Employee('Austin', 25, email);
+
+        expect(employee.email).toBe('wonkaaustin@gmail.com');
+    });
+
+});

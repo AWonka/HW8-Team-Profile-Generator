5bdb2f3606da0b8ec8b5322076527730ece160c0
Austin Wonka <wonkaaustin@gmail.com>
2022-01-11 12:05:36 -0500

    changed first test in employee.js to test if it made an object

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -2,11 +2,11 @@ const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
     it('Creates an Employee object', () => {
-        const employee = new Employee('Austin', 25, 'wonkaaustin@gmail.com'); 
+        const employee = new Employee();
+        
+        expect(typeof(employee)).toBe('object');
 
-        expect(employee.name).toEqual('Austin');
-        expect(employee.id).toEqual(25);
-        expect(employee.email).toEqual('wonkaaustin@gmail.com');
+        
 
     })
 })

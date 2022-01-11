07701d17aa1e0f29906f0870eeae59e732f0e08a
Austin Wonka <wonkaaustin@gmail.com>
2022-01-11 12:28:31 -0500

    changed it's to include get functions and tested the get functions

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -6,28 +6,30 @@ describe('Employee', () => {
         
         expect(typeof(employee)).toBe('object');
     });
-
-    it('Gives a name property to the employee object', () => {
+    // test name property
+    it('Gives a name property to the employee object and getName function returns name property', () => {
         const name = 'Austin';
         const employee = new Employee(name);
 
         expect(employee.name).toBe('Austin');
+        expect(employee.getName()).toBe('Austin');
     });
-
-    it('Gives an id property to the employee object', () => {
+    // test id property
+    it('Gives an id property to the employee object and getId function returns id property', () => {
         const id = 25;
         // give a random name as first value to cover .name property
         const employee = new Employee('Austin', id);
 
         expect(employee.id).toBe(25);
+        expect(employee.getId()).toBe(25);
     });
-
-    it('Gives an email property to the employee object', () => {
+    // test email property
+    it('Gives an email property to the employee object and getEmail function returns email property', () => {
         const email = 'wonkaaustin@gmail.com';
         // give random name and id property to fill .name and .id properties
         const employee = new Employee('Austin', 25, email);
 
         expect(employee.email).toBe('wonkaaustin@gmail.com');
+        expect(employee.getEmail()).toBe('wonkaaustin@gmail.com');
     });
-
 });

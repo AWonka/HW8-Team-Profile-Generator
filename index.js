229e6af31489037054d04b3a3de98ac1c74340e8
Austin Wonka <wonkaaustin@gmail.com>
2022-01-12 16:30:18 -0500

    added validations to each manager prompt

--- index.js
+++ index.js
@@ -14,21 +14,61 @@ const promptUser = () => {
             type: 'input',
             message: "Please enter the Manager's name.",
             name: 'name',
+            validate: name => {
+                if (name) {
+                    return true;
+                } else {
+                    console.log("Please Enter a name for the Manager!")
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             message: "Please enter the Manager's ID.",
             name: 'id',
+            validate: id => {
+                if (isNaN(id)) {
+                    console.log("Please enter an ID Number for the Manager!")
+                    return false;
+                } else if (!id) {
+                    console.log("Please enter an ID Number for the Manager!")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         },
         {
             type: 'input',
             message: "Please enter the Manager's email.",
             name: 'email',
+            validate: email => {
+                // tests email input for valid email
+                validInput = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                if (validInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter a valid Email!")
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             message: "Please enter the Manager's office number.",
             name: 'officeNumber',
+            validate: officeNumber => {
+                if (isNaN(officeNumber)) {
+                    console.log('Please enter an Office Number for the Manager!')
+                    return false;
+                } else if (!officeNumber) {
+                    console.log('Please enter an Office Number for the Manager!')
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         },
         {
             type: 'list',
